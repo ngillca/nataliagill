6ef8053ae050f96472993bda4838c2945560306a
ngillca <sngill.ca@gmail.com>
2022-02-05 12:50:05 -0800

    testing carousel

---
+++ src/images.js
@@ -0,0 +1,6 @@
+import image1 from './assets/11.png';
+import image2 from './assets/22.png';
+import image3 from './assets/33.png';
+import image4 from './assets/44.png';
+
+export default [image1, image2, image3, image4];
