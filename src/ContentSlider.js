481da8c8f6cf08654c2d2c9eedde54b5e79f289e
ngillca <sngill.ca@gmail.com>
2022-02-05 17:40:30 -0800

    hover effect

--- src/ContentSlider.js
+++ src/ContentSlider.js
@@ -6,23 +6,32 @@ export default function ContentSlider(props) {
   const [width, setWidth] = useState(0);
   const carousel = useRef();
 
-  const pic = [props.sentImages[0],props.sentImages[1],props.sentImages[2],props.sentImages[3]]
+  const pic = [
+    props.sentImages[0],
+    props.sentImages[1],
+    props.sentImages[2],
+    props.sentImages[3],
+  ];
   const obj = [
     {
       name: props.projectNames[0],
       image: props.sentImages[0],
+      url: "google.com",
     },
     {
       name: props.projectNames[1],
       image: props.sentImages[1],
+      url: "google.com",
     },
     {
       name: props.projectNames[2],
       image: props.sentImages[2],
+      url: "google.com",
     },
     {
       name: props.projectNames[3],
       image: props.sentImages[3],
+      url: "google.com",
     },
   ];
 
@@ -32,7 +41,7 @@ export default function ContentSlider(props) {
   return (
     <>
       <motion.div className="flex-container-subsetheading" animate={{ x: 10 }}>
-        <h3>{props.title}</h3>
+        <h2>{props.title}</h2>
       </motion.div>
 
       <motion.div
@@ -47,19 +56,22 @@ export default function ContentSlider(props) {
         >
           {obj.map((o) => {
             return (
-              <motion.div className="item zoom test-image" key={o}>
-                <p>{o.name}</p>
-                <img
-                  src={o.image}
-                  alt=""
-                  style={{ width: "200px", height: "200px" }}
-                />
+              <motion.div className="item " key={o}>
+        
+                  <img
+                    src={o.image}
+                    alt=""
+                    style={{ width: "200px", height: "200px" }}
+                  />
+                
+   <a href={o.url}><p className="zoom">{o.name}</p></a>
+                
+               
               </motion.div>
             );
           })}
-          
         </motion.div>
-        
+
         <motion.div></motion.div>
       </motion.div>
     </>
